7d114263cba30c7a400c9bbb5870f1ecc0e76d80
Sebastian906 <elsebas1912@gmail.com>
2026-04-21 16:08:13 -0500

    Moficaciones puntuales y actualización en el backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        dbName: 'Personal-Blog',
+        connectionFactory: (connection) => {
+          if (connection.readyState === 1) {
+            console.log('Database connected successfully', 'MongooseModule');
+          }
+          connection.on('connected', () => {
+            console.log('Database connected', 'MongooseModule');
+          });
+          connection.on('error', (err) => {
+            console.error(`Database connection failed: ${err}`, 'MongooseModule');
+          });
+          return connection;
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
